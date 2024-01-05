ccdfa63cd2d4a31b80913915fee10fb4c3557293
IsHatake <isaacramos191969@gmail.com>
2024-01-04 23:27:33 -0600

    Login FrontEnd

--- FrontEnd/app_loteria/lib/screens/login_screen.dart
+++ FrontEnd/app_loteria/lib/screens/login_screen.dart
@@ -5,6 +5,8 @@ import 'package:shared_preferences/shared_preferences.dart';
 //import 'package:simexpro/screens/recover_password_screen.dart';
 //import 'package:simexpro/widgets/navbar_roots.dart';
 import 'package:http/http.dart' as http;
+import 'package:app_loteria/utils/ColorPalette.dart';
+import 'package:google_fonts/google_fonts.dart';
 //import 'package:simexpro/api.dart';
 //import 'package:simexpro/toastconfig/toastconfig.dart';
 
@@ -13,56 +15,6 @@ class loginScreen extends StatefulWidget {
   State<loginScreen> createState() => _loginScreenState();
 }
 
-// Future<void> fetchData(
-//     BuildContext context, String username, String password) async {
-//   try {
-//     final tarea = {'usua_Nombre': username, 'usua_Contrasenia': password};
-//     final jsonTarea = jsonEncode(tarea);
-//     final response = await http.post(
-//       Uri.parse('${apiUrl}Usuarios/Login'),
-//       headers: {
-//         'XApiKey': apiKey,
-//         'Content-Type': 'application/json',
-//       },
-//       body: jsonTarea,
-//     );
-//     if (response.statusCode == 200) {
-//       print(response);
-//       final decodedJson = jsonDecode(response.body);
-//       final data = decodedJson["data"];
-//       print(data);
-//       SharedPreferences prefs = await SharedPreferences.getInstance();
-//       prefs.setInt('usua_Id', data['usua_Id']);
-//       prefs.setString('username', data['usua_Nombre']);
-//       prefs.setString('email', data['empl_CorreoElectronico']);
-//       prefs.setString('userfullname', data['emplNombreCompleto']);
-//       prefs.setString('rol', data['role_Descripcion']);
-//       prefs.setBool('esAduana', data['empl_EsAduana']);
-//       prefs.setString('image', data['usua_Image']);
-//       Navigator.push(
-//           context,
-//           MaterialPageRoute(
-//             builder: (context) => NavBarRoots(),
-//           ));
-//     } else {
-//       CherryToast.error(
-//         title: Text('El usuario o contraseña son incorrectos',
-//             style: TextStyle(color: Color.fromARGB(255, 226, 226, 226)),
-//             textAlign: TextAlign.justify),
-//         borderRadius: 5,
-//       ).show(context);
-//     }
-//   } catch (e) {
-//     if (e.toString().contains('Failed host lookup')) {
-//       CherryToast.error(
-//         title: Text('No se pudo conectar al servidor',
-//             style: TextStyle(color: Color.fromARGB(255, 226, 226, 226)),
-//             textAlign: TextAlign.justify),
-//         borderRadius: 5,
-//       ).show(context);
-//     }
-//   }
-// }
 
 class _loginScreenState extends State<loginScreen> {
   bool passToggle = true;
@@ -88,34 +40,32 @@ class _loginScreenState extends State<loginScreen> {
               ),
               child: Column(
                 children: [
-                  SizedBox(height: 10),
-                  Padding(
-                    padding: const EdgeInsets.only(top: 5),
-                    child: Image.asset(
-                      "images/SIMEXPRO-V3-PNG.jpg",
-                      height: 230,
-                    ),
-                  ),
                   Padding(
                     padding: EdgeInsets.all(20),
                     child: Card(
-                      color: Colors.white,
+                      color: Color.fromARGB(0, 0, 0, 0),
                       shape: RoundedRectangleBorder(
                         borderRadius: BorderRadius.circular(10),
                       ),
                       child: Column(
                         children: [
-                          SizedBox(height: 15),
                           Padding(
-                            padding: EdgeInsets.all(18),
-                            child: Text(
-                              'INICIO DE SESIÓN',
-                              style: TextStyle(
-                                  fontWeight: FontWeight.bold,
-                                  fontSize: 30,
-                                  color: Color.fromRGBO(148, 82, 249, 1)),
+                            padding: const EdgeInsets.only(top: 5),
+                            child: Image.asset(
+                              "images/LogoBlanco.png",
+                              height: 230,
                             ),
                           ),
+                          Padding(
+                              padding: EdgeInsets.all(18),
+                              child: Text(
+                                'INICIO DE SESIÓN',
+                                style: GoogleFonts.dongle(
+                                  fontWeight: FontWeight.bold,
+                                  fontSize: 40,
+                                  color: ColorPalette.whiteColor,
+                                ),
+                              )),
                           Padding(
                             padding: const EdgeInsets.only(
                                 right: 18, left: 18, bottom: 18),
@@ -126,10 +76,18 @@ class _loginScreenState extends State<loginScreen> {
                                 });
                               },
                               decoration: InputDecoration(
-                                border: OutlineInputBorder(),
-                                label: Text("Usuario"),
-                                prefixIcon: Icon(Icons.person),
+                                enabledBorder: OutlineInputBorder(
+                                  borderSide: BorderSide(color: Colors.white),
+                                ),
+                                focusedBorder: OutlineInputBorder(
+                                  borderSide: BorderSide(color: Colors.white),
+                                ),
+                                label: Text("Usuario",
+                                    style: GoogleFonts.dongle(color: Colors.white)),
+                                prefixIcon:
+                                    Icon(Icons.person, color: Colors.white),
                               ),
+                              style: GoogleFonts.dongle(color: Colors.white),
                             ),
                           ),
                           Padding(
@@ -142,9 +100,16 @@ class _loginScreenState extends State<loginScreen> {
                               },
                               obscureText: passToggle ? true : false,
                               decoration: InputDecoration(
-                                border: OutlineInputBorder(),
-                                label: Text("Contraseña"),
-                                prefixIcon: Icon(Icons.key),
+                                enabledBorder: OutlineInputBorder(
+                                  borderSide: BorderSide(color: Colors.white),
+                                ),
+                                focusedBorder: OutlineInputBorder(
+                                  borderSide: BorderSide(color: Colors.white),
+                                ),
+                                label: Text("Contraseña",
+                                    style: GoogleFonts.dongle(color: Colors.white)),
+                                prefixIcon:
+                                    Icon(Icons.key, color: Colors.white),
                                 suffixIcon: InkWell(
                                   onTap: () {
                                     if (passToggle == true) {
@@ -155,10 +120,13 @@ class _loginScreenState extends State<loginScreen> {
                                     setState(() {});
                                   },
                                   child: passToggle
-                                      ? Icon(CupertinoIcons.eye_slash_fill)
-                                      : Icon(CupertinoIcons.eye_fill),
+                                      ? Icon(CupertinoIcons.eye_slash_fill,
+                                          color: Colors.white)
+                                      : Icon(CupertinoIcons.eye_fill,
+                                          color: Colors.white),
                                 ),
                               ),
+                              style: GoogleFonts.dongle(color: Colors.white),
                             ),
                           ),
                           Padding(
@@ -178,10 +146,10 @@ class _loginScreenState extends State<loginScreen> {
                                   },
                                   child: Text(
                                     "¿Contraseña olvidada?",
-                                    style: TextStyle(
-                                      fontSize: 18,
+                                    style: GoogleFonts.dongle(
+                                      fontSize: 20,
                                       fontWeight: FontWeight.normal,
-                                      color: Color.fromRGBO(79, 70, 229, 1),
+                                      color: ColorPalette.whiteColor,
                                     ),
                                   ),
                                 ),
@@ -194,7 +162,7 @@ class _loginScreenState extends State<loginScreen> {
                               onTap: () {
                                 if (username.isNotEmpty &&
                                     password.isNotEmpty) {
-                               //   fetchData(context, username, password);
+                                  // fetchData(context, username, password);
                                 } else {
                                   // CherryToast.warning(
                                   //   title: Text(
@@ -212,8 +180,8 @@ class _loginScreenState extends State<loginScreen> {
                                     vertical: 10, horizontal: 20),
                                 width: double.infinity,
                                 decoration: BoxDecoration(
-                                  color: Color.fromRGBO(79, 70, 229, 1),
-                                  borderRadius: BorderRadius.circular(50),
+                                  color: ColorPalette.primaryColorApp,
+                                  borderRadius: BorderRadius.circular(8),
                                   boxShadow: [
                                     BoxShadow(
                                       color: Colors.black12,
@@ -225,9 +193,8 @@ class _loginScreenState extends State<loginScreen> {
                                 child: Center(
                                   child: Text(
                                     "Iniciar sesión",
-                                    style: TextStyle(
-                                      fontSize:
-                                          18, // Modifica el tamaño de la fuente
+                                    style: GoogleFonts.dongle(
+                                      fontSize: 25,
                                       fontWeight: FontWeight.bold,
                                       color: Colors.white,
                                     ),
@@ -236,7 +203,6 @@ class _loginScreenState extends State<loginScreen> {
                               ),
                             ),
                           ),
-                          SizedBox(height: 15)
                         ],
                       ),
                     ),
---
+++ FrontEnd/app_loteria/lib/utils/colorPalette.dart
@@ -0,0 +1,10 @@
+import 'package:flutter/material.dart';
+
+class ColorPalette {
+  static const Color primaryColorApp = Color.fromARGB(255, 0, 190, 117);
+  static const Color whiteColor = Color.fromARGB(255, 255, 255, 255);
+
+  static const Color primaryColor = Color(0xFF6200EA);
+  static const Color secondaryColor = Color(0xFF03DAC6);
+  static const Color dangerColor = Color(0xFFFF4081);
+}
